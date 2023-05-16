2682ff2a4282dbfe93c874d8397a9464c27c328e
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-05-16 16:56:37 +0300

    mobile-menu

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,18 @@
+const headerRefs = {
+  menuBtn: document.querySelector('[data-menu-btn]'),
+  menu: document.querySelector('[data-menu]'),
+  logo: document.querySelector('.site-header__logo'),
+  iconClose: document.querySelector('#icon-close'),
+  iconMenu: document.querySelector('#icon-menu'),
+};
+
+headerRefs.menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  Object.values(headerRefs)
+    .slice(0, 3)
+    .forEach(el => el.classList.toggle('is-open'));
+
+  headerRefs.iconClose.classList.toggle('visually-hidden');
+  headerRefs.iconMenu.classList.toggle('visually-hidden');
+}
